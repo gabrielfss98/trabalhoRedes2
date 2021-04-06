4fbd61c85ca497ed5a962be264283e18dadc6d68
Luiznfe <65305143+Luiznfe@users.noreply.github.com>
2021-04-06 15:35:13 -0300

    update luiz

--- client/client.py
+++ client/client.py
@@ -7,35 +7,42 @@ class Client:
         self.server_adress = (host, port)
         self.data = None
         self.server = None
+        self.file = None
         # criando socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     
     # envia o número correspondente ao aqruivo desejado
-    def send_data(self, num):
-        teste = ''
+    def request_file(self):
+        print('Digite o número do arquivo')
+        file_name = str(input())
         try:
             # enviado solicitação
-            print(f'Solicitando arquivo {num} ')
-            sent = self.socket.sendto(num, self.server_adress)
+            print(f'Solicitando arquivo {file_name} ')
+            sent = self.socket.sendto(file_name.encode(), self.server_adress)
             # recebendo resposta
-            self.data, self.server = self.socket.recvfrom(4096)
-            print(f'{self.data}')
+            self.file, self.server = self.socket.recvfrom(4096)
+            self.file = self.file.decode('utf-8')
+            print(f'{self.file}')
         finally:
             self.socket.close()
     
     def see_files(self):
-        sent = self.socket.sendto(b'archieves', self.server_adress)
+        try:
+            sent = self.socket.sendto(b'archieves', self.server_adress)
             # recebendo resposta
-        self.data, self.server = self.socket.recvfrom(4096)
-        print('Arquivos disponíveis para download: ')
-        files = str(self.data)
-        files = files.replace('b', '')
-        files = files.split(' ')
-        print(files)
-
+            self.data, self.server = self.socket.recvfrom(4096)
+            print('Arquivos disponíveis para download: ')
+            # convertendo bytes para txt
+            files = self.data.decode('utf-8').split(' ')
+            # imprimindo o nome dos arquivos 
+            for i in files:
+                print(i)
+        except :
+            print('erro see_files')
             
 c = Client('localhost', 1998)
 c.see_files()
+c.request_file()
 #c.send_data(b'archieves')
 
--- server/server.py
+++ server/server.py
@@ -12,16 +12,32 @@ def name_files(address):
         k = str(i+1)
         name = name +  f'arquivo_{k}.txt' + ' '
     skt.sendto(name.encode(), address)
-        
-        
 
-
+def return_file(num):
+    path = f'./server/arquivo_{num}.txt'
+    with open(path, 'rb') as f:
+        contents = f.read()
+    print('Enviando arquivo')
+    skt.sendto(contents, address)
+        
 while True:
     print('Aguardando requisições ...')
     data, address = skt.recvfrom(4096)
     print(f'Recebidos {len(data)} bytes de {address}')
+    # se a mensagem recebida for 'archieves' mostra os arquivos disponíveis
     if data == b'archieves':
         name_files(address)
+        # fica esperando uma resposta
+        print('Esperando arquivo ser selecionado...')
+        file_name = skt.recvfrom(4096)
+        # convertendo bytes para string
+        file_name = file_name[0].decode('utf-8')
+        print(f'Arquivo selecionado {str(file_name)}')
+        # recuperando o arquivo selecionado
+        try:
+            return_file(file_name)
+        except:
+            print('file error')   
     else:
         skt.sendto(b'ACK', address)
         print(f'ACK enviado para {address}')
