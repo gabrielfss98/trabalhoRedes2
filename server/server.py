714773cd1fdcc63884d3778775d40289b290a2d4
Luiznfe <65305143+Luiznfe@users.noreply.github.com>
2021-04-06 11:43:50 -0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,2 +1,15 @@
 import socket
 
+skt = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket UDP
+server_address = (socket.gethostname(), 1998)   # IP do servidor e porta de comunicação
+print(f'IP dos servidor: {server_address[0]}, Porta: {server_address[1]}')
+skt.bind(server_address)
+
+
+while True:
+    print('Aguardando requisições ...')
+    data, address = skt.recvfrom(4096)
+    print(f'Recebidos {len(data)} bytes de {address}')
+    skt.sendto(b'ACK', address)
+    print(f'ACK enviado para {address}')
+    print('\n\n')
